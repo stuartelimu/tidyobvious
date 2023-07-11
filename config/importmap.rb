de1c41bb4f8e48256c956381ea88238df73f2ee7
Stuart Elimu <stuartelimu@gmail.com>
2023-07-11 18:32:23 +0300

    setup + add bulma

---
+++ config/importmap.rb
@@ -0,0 +1,8 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin "bulma", to: "https://ga.jspm.io/npm:bulma@0.9.4/bulma.sass"
